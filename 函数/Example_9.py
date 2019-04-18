8ded7b8527c220c300dd2bafade2651fb14e3695
he-tengjiao <42020688+Tengjiao-He@users.noreply.github.com>
2019-04-18 17:08:43 +0800

    H

---
+++ 函数/Example_9.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 15:27:05 2019
+
+@author: Steven
+"""
+
+# 列表添加.append()
+Name = []
+Name.append("小明")
+Name.append("小红")
+Name.append("小蓝")
+print(Name)
+
+# 列表添加.insert()
+Name.insert(1, "大明")
+print(Name)
+
+
+# 列表修改
+Name[1] = "老明"
+print(Name)
+
+# 列表删除 del语句
+del Name[1]
+
+
+# 列表删除 pop()方法
+temp = Name.pop()
+print(Name)
+
+# # 列表删除 remove()方法
+Name.remove("小明")
+print(Name)
+
+
+
+
+
+# 列表的复制1
+a = ['A', 'B', 'C']
+b = a
+b[1] = 'W'
+print(a)
+print(b)
+
+print(id(a))
+print(id(b))
+
+
+# 列表的复制2
+a = ['A', 'B', 'C']
+b = ['A', 'B', 'C']
+b[1] = 'W'
+print(a)
+print(b)
+print(id(a))
+print(id(b))
+
+# 列表的复制3
+a = ['A', 'B', 'C']
+b = a[:]
+b[1] = 'W'
+print(a)
+print(b)
+
+print(id(a))
+print(id(b))
+
+
+
+# 列表的复制4
+a = ['A', 'B', 'C']
+b = a.copy()
+b[1] = 'W'
+print(a)
+print(b)
+print(id(a))
+print(id(b))
+
+
+# 列表的作用域
+def f():
+    a[1] = "W"
+    print(id(a))
+
+a = ['A', 'B', 'C']
+print(id(a))
+f()
+print(a)
+print(id(a))
+
